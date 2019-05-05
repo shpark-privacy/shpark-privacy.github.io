fe0094c77de1e5ff637ad3ea7d93e4b5c38471eb
Seonghyun Park <shpark.privacy@gmail.com>
2019-05-05 11:14:12 +0900

    map

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const ul = document.getElementById('authors');
 const url = 'https://randomuser.me/api/?results=10';
 
 fetch(url)
-  .then(resp => {
-    console.log(JSON.stringify(resp.json()));
-    return resp.json();
-  })
+  .then(resp => resp.json())
   .then(function(data) {
-    let li = createNode('li');
-    let img = createNode('img');
-    let span = createNode('span');
-    img.src = author.picture.medium;
-    span.innerHTML = `${author.name.first} ${author.name.last}`;
-    append(li, img);
-    append(li, span);
-    append(ul, li);
+    let authors = data.results;
+    return authors.map(function (author) => {
+      let li = createNode('li'),
+          img = createNode('img'),
+          span = createNode('span');
+      img.src = author.picture.medium;
+      span.innerHTML = `${author.name.first} ${author.name.last}`;
+      append(li, img);
+      append(li, span);
+      append(ul, li);
+    })
   })
   .catch(function (error) {
     console.log(JSON.stringify(error));
